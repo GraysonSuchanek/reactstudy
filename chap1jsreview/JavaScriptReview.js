f86fc0140a2fdd9a7462b12556ea259cf3a26bc7
GraysonSuchanek <graysuch@gmail.com>
2022-08-12 10:55:48 -0700

    initial commit

---
+++ chap1jsreview/JavaScriptReview.js
@@ -0,0 +1,54 @@
+const loadFunction = () => {
+
+// const person = {
+//     name: 'Max'
+// };
+
+// const newPerson = {
+//     ...person,
+//     age: 28
+// };
+
+
+// console.log(newPerson);
+
+// const filter = (...args) => {
+//     return args.filter(el => el === 1);
+// }
+
+// console.log(filter(1, 2, 3))
+
+// const numbers = [1, 2, 3];
+// [num1, ,num3] = numbers;
+// console.log(num1, num3);
+
+// const number = 1;
+// const num2 = number;
+
+// console.log(num2);
+
+// const person = {
+//     name:'Max'
+// };
+
+// const secondPerson = {
+//     ...person
+// };
+
+// const thirdPerson = person;
+
+// person.name = 'Manu';
+
+// console.log(secondPerson);
+// console.log(thirdPerson);
+
+// const numbers = [ 1, 2, 3];
+
+// const doubleNumArray = numbers.map((num) => {
+//     return num * 2;
+// });
+
+// console.log(numbers);
+// console.log(doubleNumArray);
+
+};
