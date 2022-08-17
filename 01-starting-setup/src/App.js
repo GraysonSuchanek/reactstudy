896c3e26940837b09489544e869ca7df31cfdb2b
GraysonSuchanek <graysuch@gmail.com>
2022-08-16 23:34:18 -0700

    javascript translation

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,8 @@
 function App() {
+  // JS without React --
+  // const para = document.createElement('p');
+  // para.textContent = 'This is also visible';
+  // document.getElementById('root').append(para);
   return (
     <div>
       <h2>Let's get started!</h2>
