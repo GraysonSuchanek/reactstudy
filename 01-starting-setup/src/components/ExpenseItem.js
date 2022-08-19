1137f1831cdf95944d7119a8bc93f9ba8fedaa56
GraysonSuchanek <graysuch@gmail.com>
2022-08-19 11:28:51 -0700

    multiple props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -3,10 +3,10 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
       <div className="expense-item">
-        <div>{props[0].date.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className="expense-item__description">
-          <h2>{props[0].title}</h2>
-          <div className="expense-item__price">{props[0].amount}</div>
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
         </div>
       </div>
   );
