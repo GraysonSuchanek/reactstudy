f324dcc090a73d729c0a29f98703992030fe9ea1
GraysonSuchanek <graysuch@gmail.com>
2022-09-06 18:15:18 -0700

    implementing useState

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,15 @@
+import React, { useState } from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card"
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluted by React');
 
   const clickHandler = () => {
-    title = 'Updated!';
+    setTitle('Updated!');
     console.log(title);
   }
 
